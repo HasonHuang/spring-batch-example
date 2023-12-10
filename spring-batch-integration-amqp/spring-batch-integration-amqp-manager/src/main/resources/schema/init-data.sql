f6c3594f464575835be14789cfa1b36aae3e19cb
HasonHuang <258831020@qq.com>
2023-12-10 23:42:50 +0800

    add init scripts

---
+++ spring-batch-integration-amqp/spring-batch-integration-amqp-manager/src/main/resources/schema/init-data.sql
@@ -0,0 +1,14 @@
+insert into t_source_data (name, description)
+VALUES
+    ('a', 'a'),
+    ('b', 'b'),
+    ('c', 'c'),
+    ('d', 'd'),
+    ('e', 'e'),
+    ('f', 'f'),
+    ('g', 'g'),
+    ('h', 'h'),
+    ('i', 'i'),
+    ('j', 'j'),
+    ('k', 'k')
+;
---
+++ spring-batch-integration-amqp/spring-batch-integration-amqp-manager/src/main/resources/schema/init-db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `t_source_data` (
+    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `description` varchar(255) NULL
+);
