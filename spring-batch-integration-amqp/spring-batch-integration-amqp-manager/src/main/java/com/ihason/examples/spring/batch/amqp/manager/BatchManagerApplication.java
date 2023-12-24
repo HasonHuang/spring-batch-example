178f1f88b4f00b98f03f5f837abd034347eed9ca
HasonHuang <258831020@qq.com>
2023-12-24 20:43:21 +0800

    refactor: 移除不必要的 Nacos 注册中心

--- spring-batch-integration-amqp/spring-batch-integration-amqp-manager/src/main/java/com/ihason/examples/spring/batch/amqp/manager/BatchManagerApplication.java
+++ spring-batch-integration-amqp/spring-batch-integration-amqp-manager/src/main/java/com/ihason/examples/spring/batch/amqp/manager/BatchManagerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 批处理任务 Manager
@@ -13,7 +12,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author Hason
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BatchManagerApplication {
 
     @Autowired
--- spring-batch-integration-amqp/spring-batch-integration-amqp-worker/src/main/java/com/ihason/examples/spring/batch/amqp/worker/BatchWorkerApplication.java
+++ spring-batch-integration-amqp/spring-batch-integration-amqp-worker/src/main/java/com/ihason/examples/spring/batch/amqp/worker/BatchWorkerApplication.java
@@ -2,7 +2,6 @@ package com.ihason.examples.spring.batch.amqp.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 批处理 worker
@@ -10,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author Hason
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BatchWorkerApplication {
 
     public static void main(String[] args) {
